a383235c3c0914e64c72bfddedd96f8534710d40
Jesse <jesse1024@gmail.com>
2025-03-22 17:52:28 +0800

    12

--- main.py
+++ main.py
@@ -1,36 +1,74 @@
 import time
-from common import *  
+from common import *
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableLambda, RunnableSequence
+from langchain.schema.runnable import RunnableLambda, RunnableParallel
 
-start_time = time.time()  # 獲取開始時間
+start_time = time.time()  # 獲取開始的時間
 load_dotenv()
 
-# 訊息陣列
-print("=" * 100)
-messages = [
-    ("human", "我想學習{language}語言，請用英語回答我，並且控制在35個單字以內。"),
-]
-prompt_template = ChatPromptTemplate.from_messages(messages)
-# 大語言模型
 model = ChatOpenAI(model="gpt-4o-mini")
-# 使用Chain呼叫模型
-run_prompt = RunnableLambda(lambda x: prompt_template.invoke(x))
-run_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
-run_output = RunnableLambda(lambda x: StrOutputParser().invoke(x))
-run_uppercase = RunnableLambda(lambda x: x.upper())  # 將輸出轉換為大寫
-# run_countwords = RunnableLambda(lambda x: f"{x}\n共{len(x.split())}詞")  # 計算輸出的單字數
-run_countwords = RunnableLambda(lambda x: print(x) or len(x))  # 計算輸出的單字數
 
-# chain = RunnableSequence(first=run_prompt, middle=[run_model], last=run_output)
-# ↓
-# chain = run_prompt | run_model | run_output
-chain = run_prompt | run_model | run_output | run_uppercase | run_countwords
+print("=" * 100)
+prompt_input_chain = RunnableLambda(
+    lambda input: "編程語言: {language}".format(language=input["language"])
+)
+
+def prompt_merit(language): # 優點提示詞
+    messages = [
+        ("system", "你是一位經驗豐富的編程高手。"),
+        ("human", "我想學習{language}，請在100個單詞以內描述優點。"),
+    ]
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    return prompt_template.format_prompt(language=language)
+
+merit_branch_chain = ( # 優點分支鏈
+    RunnableLambda(lambda x: prompt_merit(x)) | model | StrOutputParser()
+)
+
+def prompt_demerit(language): # 缺點提示詞
+    messages = [
+        ("system", "你是一位經驗豐富的編程高手。"),
+        ("human", "我想學習{language}，請在100個單詞以內描述缺點。"),
+    ]
+    prompt_template = ChatPromptTemplate.from_messages(messages)
+    return prompt_template.format_prompt(language=language)
+
+demerit_branch_chain = ( # 缺點分支鏈
+    RunnableLambda(lambda x: prompt_demerit(x)) | model | StrOutputParser()
+)
+
+def combine_merit_demerit(branch_outputs): # 合併分支輸出結果
+    merit = branch_outputs["merit"]
+    demerit = branch_outputs["demerit"]
+    return f"[優點]:\n{merit}\n\n[缺點]:\n{demerit}"
+
+combine_merit_demerit_chain = RunnableLambda( # 合併分支輸出結果 Chain 物件
+    lambda x: combine_merit_demerit(x["branches"])
+)
+
+def output(x): # 僅作為中間過程的輸出
+    print(">", x)
+    return x
+
+output_chain = RunnableLambda(lambda x: output(x)) # 輸出中間結果（日誌用）
+
+# 使用 Chain 呼叫模型
+chain = (
+    prompt_input_chain |
+    output_chain |
+    RunnableParallel(
+        branches={"merit": merit_branch_chain, "demerit": demerit_branch_chain}
+    ) |
+    combine_merit_demerit_chain |
+    StrOutputParser()
+)
+
 result = chain.invoke({"language": "python"})
+# result = chain.invoke({"language": "java"})
 print(result)
 
-# 印出總耗時
-print(time.time() - start_time)
+# 列印結束時間
+print(evalEndTime(start_time))
