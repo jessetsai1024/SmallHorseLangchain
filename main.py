ebd6f103f95654008565cea7027b8053b8cca1d1
Jesse <jesse1024@gmail.com>
2025-03-22 14:29:48 +0800

    10-1

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ to_uppercase_lambda = RunnableLambda(to_uppercase)
 append_comma_lambda = RunnableLambda(append_comma)
 
 chain = to_uppercase_lambda | append_comma_lambda
-print(chain.invoke("hello world"))
+print(chain.invoke("hello world")) 
