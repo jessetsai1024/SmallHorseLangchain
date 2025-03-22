aa5fe0acf6523fa1b1b5df7fe8942e67c6752d0c
Jesse <jesse1024@gmail.com>
2025-03-22 15:59:40 +0800

    10-2

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from langchain.schema.runnable import RunnableLambda
+import time
+from common import *
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
 
-# 字母大寫
-def to_uppercase(inputs):
-    return inputs.upper()
+start_time = time.time()  # 獲取開始時間
+load_dotenv()
 
-# 前後加括號
-def append_comma(inputs):
-    return f"[[{inputs}]]"
+# 訊息陣列
+print("=" * 100)
+messages = [
+    ("system", "你是一位{career}專家，你經常輔導你的學生。"),
+    ("human", "我想學習{language}，給我幾個建議好嗎？"),
+]
 
-to_uppercase_lambda = RunnableLambda(to_uppercase)
-append_comma_lambda = RunnableLambda(append_comma)
+prompt_template = ChatPromptTemplate.from_messages(messages)
 
-chain = to_uppercase_lambda | append_comma_lambda
-print(chain.invoke("hello world")) 
+# 語言模型
+model = ChatOpenAI(model="gpt-4o-mini")
+
+# 使用Chain調用模型
+# chain = prompt_template | model | StrOutputParser()
+# ↓
+run_prompt = RunnableLambda(lambda x: prompt_template.invoke(x))
+run_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
+run_output = RunnableLambda(lambda x: StrOutputParser().invoke(x))
+
+chain = RunnableSequence(first=run_prompt, middle=[run_model], last=run_output)
+result = chain.invoke({"career": "醫學", "language": "按摩"})
+print(result)
+
+# 打印結束時間
+print(evalEndTime(start_time))
