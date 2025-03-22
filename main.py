6de8209612ca159d0ef31dbeaa6b6e7b99be1f8c
Jesse <jesse1024@gmail.com>
2025-03-22 16:22:08 +0800

    wip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from common import *
+from common import *  
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
@@ -12,25 +12,25 @@ load_dotenv()
 # 訊息陣列
 print("=" * 100)
 messages = [
-    ("system", "你是一位{career}專家，你經常輔導你的學生。"),
-    ("human", "我想學習{language}，給我幾個建議好嗎？"),
+    ("human", "我想學習{language}語言，請用英語回答我，並且控制在35個單字以內。"),
 ]
-
 prompt_template = ChatPromptTemplate.from_messages(messages)
-
-# 語言模型
+# 大語言模型
 model = ChatOpenAI(model="gpt-4o-mini")
-
-# 使用Chain調用模型
-# chain = prompt_template | model | StrOutputParser()
-# ↓
+# 使用Chain呼叫模型
 run_prompt = RunnableLambda(lambda x: prompt_template.invoke(x))
 run_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
 run_output = RunnableLambda(lambda x: StrOutputParser().invoke(x))
+run_uppercase = RunnableLambda(lambda x: x.upper())  # 將輸出轉換為大寫
+# run_countwords = RunnableLambda(lambda x: f"{x}\n共{len(x.split())}詞")  # 計算輸出的單字數
+run_countwords = RunnableLambda(lambda x: print(x) or len(x))  # 計算輸出的單字數
 
-chain = RunnableSequence(first=run_prompt, middle=[run_model], last=run_output)
-result = chain.invoke({"career": "醫學", "language": "按摩"})
+# chain = RunnableSequence(first=run_prompt, middle=[run_model], last=run_output)
+# ↓
+# chain = run_prompt | run_model | run_output
+chain = run_prompt | run_model | run_output | run_uppercase | run_countwords
+result = chain.invoke({"language": "python"})
 print(result)
 
-# 打印結束時間
-print(evalEndTime(start_time))
+# 印出總耗時
+print(time.time() - start_time)
