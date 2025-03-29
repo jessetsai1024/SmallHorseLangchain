abced463fb175ed3390dd441ad4deb7821546ab1
Jesse <jesse1024@gmail.com>
2025-03-29 08:59:30 +0800

    20

--- main.py
+++ main.py
@@ -3,79 +3,61 @@ from common import *
 from dotenv import load_dotenv
 
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 print("=" * 100)
-
-# 定義client_prompt
-# client_prompt = "請問桃園結義是幾個人？都是誰？"
-# client_prompt = "虎牢關是誰打敗了呂布嗎？"
-client_prompt = "孔明是怎麼罵死曹真的？把內封信的原文找出來"
-# client_prompt = "劉備三顧茅廬請出的人是誰？"
-# client_prompt = "川普是哪一國總統?"
 
 start_time = time.time()  # 獲取開始時間
 load_dotenv()
 
-chroma_dbpath = os.path.join(os.path.dirname(__file__), "db/sanguo.db")
+db = Chroma(
+    persist_directory = os.path.join(os.path.dirname(__file__), "db/sanguo.db"), 
+    embedding_function = OpenAIEmbeddings(model="text-embedding-3-small")
+)
+model = ChatOpenAI(model="gpt-4o")
+retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
 
-if not os.path.exists(chroma_dbpath):
-    print(">", f"未找到儲存路徑:{chroma_dbpath}")
-    exit(0)
-
-# 定義OpenAIEmbeddings
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
-# 定義Chroma
-db = Chroma(persist_directory=chroma_dbpath, embedding_function=embeddings)
-
-# 定義retriever
-# - https://python.langchain.com/v0.2/docs/integrations/vectorstores/chroma/
-# retriever_docs = db.similarity_search_with_score(client_prompt)
-retriever = db.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": 1},
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "請根據「參考文檔」回答問題，如果在這個參考文檔中沒有找到答案，請回答“不知道”。"),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    model, retriever, contextualize_q_prompt
 )
 
-retriever_docs = retriever.invoke(client_prompt)
-
-print(">", "查詢文件:", len(retriever_docs))
-# for i, doc in enumerate(retriever_docs):
-#     print(f"{i+1}. {doc.page_content}")
-
-if len(retriever_docs) == 0:
-    print(">", "未找到相關文件")
-    exit(0)
-
-human_prompt = """請根據提供的 `參考文件` 回答下面的問題：
-
-問題: {client_prompt}
-
-參考文件：
-\"\"\"
-{reference_docs}
-\"\"\"
-
-請根據 `參考文件` 回答問題，如果在這個參考文件中沒有找到答案，請回答“不知道”。
-""".format(
-    client_prompt=client_prompt,
-    reference_docs="\n".join([doc.page_content for doc in retriever_docs]),
+system_prompt = (
+    "請根據「參考文檔」回答問題，如果在這個參考文檔中沒有找到答案，請回答“不知道”。"
+    "\n\n"
+    "{context}"
 )
-print(human_prompt)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(model, prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-messages = [
-    SystemMessage(
-        "請嚴格按照提供的參考文件回答使用者的問題，不要引用參考文件之外的內容。"
-    ),
-    HumanMessage(human_prompt),
-]
+chat_history = []  # 收集聊天歷史（訊息序列）
+while True:
+    query = input(": ")
+    if query.lower() == "exit":
+        break
+    print("\r> 正在檢索答案...", end="")
+    result = rag_chain.invoke({"input": query, "chat_history": chat_history})
+    print("\r>", f"{result['answer']}")
 
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-
-print("==========================================================")
-result = model.invoke(messages)
-print(result.content)
+    chat_history.append(HumanMessage(content=query))
+    chat_history.append(SystemMessage(content=result["answer"]))
 
 print(evalEndTime(start_time))
