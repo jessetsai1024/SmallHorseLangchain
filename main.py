46e05171c6d2647b98d6793007d15300bba5d633
Jesse <jesse1024@gmail.com>
2025-04-04 11:06:22 +0800

    27

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pprint, time
 from langchain import hub
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain_core.tools import StructuredTool
+from langchain.tools import tool
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from common import *
@@ -12,53 +12,26 @@ print("=" * 100)
 start_time = time.time()  # 取得開始時間
 load_dotenv()
 
+@tool()
 def say_hello(name: str) -> str:
     """
     Say hello to a person
     """
     return f"親愛的{name}，你好！"
 
-class SayHelloInput(BaseModel):
-    name: str = Field(..., title="The name of the person to say hello to")
+class ReverseStringInput(BaseModel):
+    content: str = Field(..., title="The string to reverse")
 
+@tool(args_schema=ReverseStringInput)
 def reverse_string(content: str) -> str:
     """
     Reverse a string
     """
     return content[::-1]
 
-class ReverseStringInput(BaseModel):
-    content: str = Field(..., title="The string to reverse")
-
-def concatenate_strings(a: str, b: str) -> str:
-    """
-    Concatenate two strings
-    """
-    return a + b
-
-class ConcatenateStringsInput(BaseModel):
-    a: str = Field(..., title="The first string")
-    b: str = Field(..., title="The second string")
-
 tools = [
-    StructuredTool.from_function(
-        func=say_hello,
-        args_schema=SayHelloInput,
-        output_field_name="greeting",
-        description="Say hello to a person",
-    ),
-    StructuredTool.from_function(
-        func=reverse_string,
-        args_schema=ReverseStringInput,
-        output_field_name="reversed_string",
-        description="Reverse a string",
-    ),
-    StructuredTool.from_function(
-        func=concatenate_strings,
-        args_schema=ConcatenateStringsInput,
-        output_field_name="result_string",
-        description="Concatenate two strings",
-    ),
+    say_hello,
+    reverse_string,
 ]
 
 # 建立LLM
@@ -66,7 +39,6 @@ llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
 # ReAct提示詞
 prompt = hub.pull("hwchase17/openai-tools-agent")
-# prompt.pretty_print()
 
 # 建立Agent
 agent = create_tool_calling_agent(
@@ -83,22 +55,16 @@ agent_executor = AgentExecutor.from_agent_and_tools(
     handle_parsing_errors=True,
 )
 
-############################################################
+##############################################
 
-# 調用Agent
+# 呼叫Agent
 response = agent_executor.invoke({"input": "我是小馬"})
 pprint.pprint(response)
 
-response = agent_executor.invoke({"input": "我想翻轉一下這個字串：Hello"})
+response = agent_executor.invoke({"input": "我想翻轉一下這個字串：Youtube"})
 pprint.pprint(response)
 
-response = agent_executor.invoke(
-    {
-        "input": """
-我想把這兩個字串連接起來：
-Hello
-World
-"""
-    }
-)
-pprint.pprint(response)
+##############################################
+
+# 列印結束時間
+print("\n", evalEndTime(start_time))
