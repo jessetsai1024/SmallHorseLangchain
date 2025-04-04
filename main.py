59e42824b72fda4666747330a2d859a6f38a3e77
Jesse <jesse1024@gmail.com>
2025-04-04 12:44:03 +0800

    29

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pprint, time
+
 from typing import Type
 from langchain import hub
 from langchain.agents import AgentExecutor, create_tool_calling_agent
@@ -9,28 +10,34 @@ from dotenv import load_dotenv
 from common import *
 
 print("=" * 100)
-start_time = time.time()  # 取得開始時間
+start_time = time.time()     # 獲取開始時間
 load_dotenv()
 
-class MultiplyToolArgs(BaseModel):
-    a: int = Field(..., title="First number")
-    b: int = Field(..., title="Second number")
+class SimpleSearchToolArgs(BaseModel):
+    query: str = Field(..., title="Query string")
 
-class MultiplyTool(BaseTool):
-    name = "multiply"
-    description = "Multiply two numbers together"
-    args_schema: Type[BaseModel] = MultiplyToolArgs
+class SimpleSearchTool(BaseTool):
+    name = "simple_search"
+    description = "A simple search tool"
+    args_schema: Type[BaseModel] = SimpleSearchToolArgs
 
-    def _run(self, a: int, b: int):
-        """Multiply two numbers together"""
-        print(f"Multiplying {a} by {b}")
-        # return {"result": a * b}
-        return {"result": 12345}  # 故意返回一個錯誤的結果
+    def _run(self, query: str):
+        """
+        Run the search tool
+        執行搜尋工具
+        """
+        from tavily import TavilyClient
 
-# 工具數組
-tools = [MultiplyTool()]
+        # client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+        client = TavilyClient(api_key="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")
+        results = client.search(query)
+        return results
+        # return f"search results for {query}\n\n{results}\n"
 
-# pprint.pprint(tools[0]._run(a=2, b=3))
+tools = [SimpleSearchTool()]
+
+# pprint.pprint(tools[0]._run("python"))
+# pprint.pprint(tools[0]._run("我想找一些Python的工作"))
 
 # 建立LLM
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
@@ -53,14 +60,13 @@ agent_executor = AgentExecutor.from_agent_and_tools(
     handle_parsing_errors=True,
 )
 
-##############################################
-
+############################################################
 # 呼叫Agent
-response = agent_executor.invoke(
-    {"input": "計算一下 2289 乘以 39098 等於多少？"}
-)
-# echo "2289 * 39098" | bc
-pprint.pprint(response)
+# response = agent_executor.invoke({"input": "我想找一些Python的工作"})
+response = agent_executor.invoke({"input": "今天東京的天氣如何？"})
+# response = agent_executor.invoke({"input": "1+1=?"})
+print(response["output"])
 
-# 列印結束時間
+############################################################
+# 打印結束時間
 print("\n", evalEndTime(start_time))
